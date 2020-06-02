2764f996fd8241eb01fdd1b624e67300ffdf23a6
patrycja.czerwinska <patison89@gmail.com>
2020-06-02 13:02:20 +0100

    add fontawesome

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,13 @@ import App from "./App.vue";
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faDna } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
+library.add(faDna);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.config.productionTip = false;
 
 new Vue({
